a8804cc224b1225f8246facbef4eddcde8e208cc
Sorawit yoktlemtae <Sorawit_yok@cmu.ac.th>
2023-12-19 15:52:05 +0700

    com p

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,27 +1,24 @@
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <cmath>
-
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<cmath>
+#include<iomanip>
 using namespace std;
-double sum = 0;
-int count = 0;
-double mean,StandardDeviation,num_2;
-int main(){
-    ifstream source;
-    source.open("score.txt");
-    string textline;
-    while (getline(source,textline))
-    {
-        sum += atof(textline.c_str());
-        num_2 += pow(atof(textline.c_str()),2);
-        count++;
-    }
-    mean = sum/count;
-    StandardDeviation = sqrt((num_2/count)-pow(mean,2));
 
-    cout << "Number of data = " << count << endl;
+int main(){
+    ifstream source("score.txt");
+    string text;
+    float i=0,sum=0,stdf=0,std;
+    
+    while(getline(source,text)){
+        sum += atof(text.c_str());
+        stdf += pow(atof(text.c_str()),2);
+        i++;
+	}
+    std = sqrt((stdf/i)-pow(sum/i,2));
+    cout << "Number of data = " << i << endl;
     cout << setprecision(3);
-    cout << "Mean = " << mean << endl;
-    cout << "Standard deviation = " << StandardDeviation;
+    cout << "Mean = " << sum/i << endl;
+    cout << "Standard deviation = " << std;
 }
